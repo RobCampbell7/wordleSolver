c939a8dd0eac26fa93a6be902dba3dfa57eec36a
robCampbell209 <rhc209@exeter.ac.uk>
2025-01-07 17:08:47 +0000

    tidying up functions

--- parameterSelection/paramSelectVisual.py
+++ parameterSelection/paramSelectVisual.py
@@ -1,24 +1,10 @@
 from copy import deepcopy
 import csv
+from functools import cache
+from win11toast import toast
 from matplotlib.animation import FuncAnimation, FFMpegWriter
 import numpy as np
 import matplotlib.pyplot as plt
-import matplotlib as mpl
-import multiprocessing as mp
-
-# mpl.use('Qt5Agg')
-mpl.rcParams['animation.ffmpeg_path'] = r'C:\\Users\\rhc209\\OneDrive - University of Exeter\\python\\ffmpeg-master-latest-win64-gpl\\bin\\ffmpeg.exe'
-k = -1
-def update(frame):
-    # global k
-    # k += 1
-    # print(frame)
-    # print("k = " + repr(k))
-    plot.set_array(data[frame].ravel())
-    # print(*data, sep = ",\n")
-    # plot2.set_data([x for x, y in swarm.particlePositions()],
-    #                [y for x, y in swarm.particlePositions()])
-    return plot,
 
 # distanceHash = {}
 # def dist(x1, y1, x2, y2):
@@ -26,31 +12,33 @@ def update(frame):
 #         distanceHash[(x1, y1, x2, y2)] = ((x1-x2)**2 + (y1-y2)**2)**0.5
 #     return distanceHash[(x1, y1, x2, y2)]
 
-# distanceHash = {}
-# def dist2(x1, y1, x2, y2):
-#     if distanceHash.get((x1, y1, x2, y2), None) == None:
-#         distanceHash[(x1, y1, x2, y2)] = (x1-x2)**2 + (y1-y2)**2
-#     return distanceHash[(x1, y1, x2, y2)]
-def dist2(x1, y1, x2, y2):
+@cache
+def dist2Calc(x1, y1, x2, y2):
     return (x1-x2)**2 + (y1-y2)**2
+
+def dist2(x1, y1, x2, y2):
+    if x1 < x2:
+        return dist2Calc(x1, y1, x2, y2)
+    else:
+        return dist2Calc(x2, y2, x1, y1)
 
 def manhattan(x1, x2, y1, y2):
     return x1 + y1 - x2 - y2
 
-def nearestValue(x, y, valueDict):
+def nearestValue(x, y):
     return valueDict[min(valueDict.keys(), key=lambda c : dist2(x, y, *c))]
 
-def voronoi(valueDict):
-    xField, yField = np.meshgrid(np.linspace(0, 1, 500), np.linspace(0, 1, 500))
-    zField = np.vectorize(lambda x, y : nearestValue(x, y, valueDict))(xField, yField)
-    zField = zField[:-1, :-1]
-    return zField
+# def voronoi(valueDict):
+#     xField, yField = (np.linspace(0.0, 1.0, 50), np.linspace(0.0, 0.1, 50))
+#     zField = np.vectorize(lambda x, y : nearestValue(x, y, valueDict))(xField, yField)
+#     zField = zField[:-1, :-1]
+#     return zField
 
 # def findVoronoi(row):
 
 particlePositions = []
 data = []
-rawData = []
+# rawData = []
 valueDictonaries = []
 with open("particlePositions.csv", "r") as posFile:
     valueDict = {}
@@ -60,21 +48,27 @@ with open("particlePositions.csv", "r") as posFile:
 
             for i in range(len(row) // 3):
                 valueDict[(float(row[3 * i]), float(row[3 * i + 1]))] = float(row[3 * i + 2])
-            rawData.append(deepcopy(valueDict))
+            # rawData.append(deepcopy(valueDict))
             # data.append(voronoi(valueDict)[2])
             # particlePositions.append([float(x) for x in row])
 
+xBounds = [0, 1]
+yBounds = [0, 1]
+nX = 100
+nY = 100
+
+
 if __name__=="__main__":
-    with mp.Pool(4) as p:
-        data = p.map(voronoi, rawData)
+    x, y = np.meshgrid(np.linspace(*xBounds, nX), np.linspace(*yBounds, nY))
+    data = np.vectorize(nearestValue)(x, y)
+    toast("Done!")
     input("Press enter to continue...")
 
     fig, ax = plt.subplots(figsize=(4, 4))
-    xField, yField = np.meshgrid(np.linspace(0, 1, 500), np.linspace(0, 1, 500))
-    plot = plt.pcolormesh(xField, yField, data[0], cmap='inferno_r')#, vmin = 2, vmax = 10)
+    plot = plt.pcolormesh(x, y, data, cmap='inferno_r')#, vmin = 2, vmax = 10)
+    # plot = plt.pcolormesh(xField, yField, data[0], cmap='inferno_r')#, vmin = 2, vmax = 10)
     # plot2, = plt.plot([x for x, y in swarm.particlePositions()],
     #                   [y for x, y in swarm.particlePositions()], 
     #                   "b.", lw=2)
     # # allPlots = (plot1, plot2)
-    ani = FuncAnimation(fig, update, frames=len(data), interval=250, blit=True, repeat=False)#, init_func=init)
     plt.show()
--- parameterSelection/singleObjParamSelect.py
+++ parameterSelection/singleObjParamSelect.py
@@ -17,15 +17,11 @@ def objective(x):
     return x
 
 def surrogate(model, X):
-	# catch any warning generated when making a prediction
 	with catch_warnings():
-		# ignore generated warnings
 		simplefilter("ignore")
 		return model.predict(X, return_std=True)
 
-# probability of improvement acquisition function
 def acquisition(X, Xsamples, model):
-	# calculate the best surrogate score found so far
 	yhat, _ = surrogate(model, X)
 	best = min(yhat)
 	# calculate mean and stdev via surrogate function
@@ -35,8 +31,7 @@ def acquisition(X, Xsamples, model):
 	probs = norm.cdf((mu - best) / (std+1E-9))
 	return probs
  
-# optimize the acquisition function
-def opt_acquisition(X, y, model):
+def optAcquisition(X, y, model):
 	# random search, generate random samples
 	Xsamples = np.random.random(100)
 	Xsamples = Xsamples.reshape(len(Xsamples), 1)
@@ -46,7 +41,6 @@ def opt_acquisition(X, y, model):
 	ix = np.argmax(scores)
 	return Xsamples[ix, 0]
  
-# plot real observations vs surrogate function
 def plot(X, y, model):
 	# scatter plot of inputs and real objective function
 	plt.plot(X, y, "rx")
@@ -59,33 +53,27 @@ def plot(X, y, model):
 	plt.show()
 
 if __name__=="__main__":
-    # sample the domain sparsely with noise
     X = np.random.random(100)
     y = np.asarray([objective(x) for x in X])
     print("Domain sampled")
-    # reshape into rows and cols
+
     X = X.reshape(len(X), 1)
     y = y.reshape(len(y), 1)
-    # define the model
+
     model = GaussianProcessRegressor()
-    # fit the model
     model.fit(X, y)
-    # plot before hand
-    # plot(X, y, model)
-    # perform the optimization process
+
     for i in range(100):
-        x = opt_acquisition(X, y, model)
+        x = optAcquisition(X, y, model)
         actual = objective(x)
         est, _ = surrogate(model, [[x]])
         print("i={0}, x={1:.3f}, f()={2:3f}, actual={3:.3f}, c={4}".format(i, x, est[0], actual, COUNT))
-        # add the data to the dataset
+
         X = np.vstack((X, [[x]]))
         y = np.vstack((y, [[actual]]))
-        # update the model
-        model.fit(X, y)
-    # plot all samples and the final surrogate function
-    print(len(X))
     
+        model.fit(X, y)
+
     plot(X, y, model)
     # best result
     ix = np.argmin(y)
